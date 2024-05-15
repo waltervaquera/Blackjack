2311de2ab2f1550737fb4ae9720963dcdeb5231c
btt <waltervaquera17@gmail.com>
2024-05-15 14:33:01 -0400

    Add the Card, Deck and Hand classes with their respective functionalities

---
+++ Blackjack/Classes/Deck.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+
+namespace BlackjackConsoleGame.Classes
+{
+    class Deck
+    {
+        private List<Card> cards;
+        private Random random;
+
+        public Deck()
+        {
+            cards = new List<Card>();
+            random = new Random();
+
+            for (int suit = 0; suit <= 3; suit++)
+            {
+                for (int value = 1; value <= 13; value++)
+                {
+                    cards.Add(new Card((Suits)suit, (Values)value));
+                }
+            }
+        }
+
+        public void Shuffle()
+        {
+            List<Card> shuffledCards = new List<Card>(cards);
+            cards.Clear();
+
+            while (shuffledCards.Count > 0)
+            {
+                int randomIndex = random.Next(shuffledCards.Count);
+                cards.Add(shuffledCards[randomIndex]);
+                shuffledCards.RemoveAt(randomIndex);
+            }
+        }
+
+        public Card DrawCard()
+        {
+            if (cards.Count == 0)
+            {
+                throw new Exception("Out of cards!");
+            }
+
+            Card cardToDraw = cards[0];
+            cards.RemoveAt(0);
+            return cardToDraw;
+        }
+    }
+}
---
+++ Blackjack/Classes/Hand.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+
+namespace BlackjackConsoleGame.Classes
+{
+    class Hand : List<Card>
+    {
+        public override string ToString()
+        {
+            string handString = "";
+            foreach (Card card in this)
+            {
+                handString += $"{card}\n";
+            }
+            return handString;
+        }
+
+        public string ToString(bool hideFirstCard)
+        {
+            string handString = "";
+            if (hideFirstCard)
+            {
+                handString += "Hidden\n";
+            }
+            else
+            {
+                handString += this[0] + "\n";
+            }
+
+            for (int i = 1; i < this.Count; i++)
+            {
+                handString += this[i] + "\n";
+            }
+
+            return handString;
+        }
+    }
+}
