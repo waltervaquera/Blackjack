335d80251e56a648018b0671e3b0dfb4b5dc53e1
btt <waltervaquera17@gmail.com>
2024-05-15 14:37:00 -0400

    Add Player and Dealer classes

---
+++ Blackjack/Classes/Dealer.cs
@@ -0,0 +1,13 @@
+﻿namespace BlackjackConsoleGame.Classes
+{
+    class Dealer : Player
+    {
+        public void PlayHand(Deck deck)
+        {
+            while (GetScore() < 17)
+            {
+                Hand.Add(deck.DrawCard());
+            }
+        }
+    }
+}
---
+++ Blackjack/Classes/Player.cs
@@ -0,0 +1,45 @@
+﻿namespace BlackjackConsoleGame.Classes
+{
+    class Player
+    {
+        public Hand Hand { get; set; }
+
+        public Player()
+        {
+            Hand = new Hand();
+        }
+
+        public int GetScore()
+        {
+            int score = 0;
+            int acesCount = 0;
+
+            foreach (Card card in Hand)
+            {
+                if (card.Value == Values.Ace)
+                {
+                    acesCount++;
+                }
+                else
+                {
+                    score += (int)card.Value;
+                }
+            }
+
+            while (acesCount > 0)
+            {
+                if (score + 11 <= 21)
+                {
+                    score += 11;
+                }
+                else
+                {
+                    score += 1;
+                }
+                acesCount--;
+            }
+
+            return score;
+        }
+    }
+}
