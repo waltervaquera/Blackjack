0e7f5fbb96700dcda8aad5807e0ccc1c569c2c95
btt <waltervaquera17@gmail.com>
2024-05-28 12:59:25 -0400

    implement game controller

---
+++ BlackjackAPI/Controllers/BlackjackController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using BlackjackAPI.Classes;
+
+namespace BlackjackAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BlackjackController : Controller
+    {
+        private GameManager gameManager;
+
+        public BlackjackController()
+        {
+            gameManager = new GameManager();
+        }
+
+        [HttpPost("start")]
+        public IActionResult StartGame()
+        {
+            gameManager.StartGame();
+            return Ok();
+        }
+
+        [HttpGet("player-hand")]
+        public IActionResult GetPlayerHand()
+        {
+            return Ok(gameManager.GetPlayerHand());
+        }
+
+        [HttpGet("dealer-hand")]
+        public IActionResult GetDealerHand()
+        {
+            return Ok(gameManager.GetDealerHand());
+        }
+
+        [HttpPost("hit")]
+        public IActionResult Hit()
+        {
+            gameManager.Hit();
+            return Ok();
+        }
+
+        [HttpPost("stand")]
+        public IActionResult Stand()
+        {
+            GameResult gameResult = gameManager.Stand();
+            return Ok(gameResult);
+        }
+    }
+}
