e23f1096518933f054d2a1107a087e311d82e207
btt <waltervaquera17@gmail.com>
2024-05-28 12:45:11 -0400

    implement the basic classes of the game

---
+++ BlackjackAPI/Classes/Hand.cs
@@ -0,0 +1,17 @@
+﻿using System.Text;
+
+namespace BlackjackAPI.Classes
+{
+    public class Hand : List<Card>
+    {
+        public override string ToString()
+        {
+            StringBuilder handString = new StringBuilder();
+            foreach (Card card in this)
+            {
+                handString.AppendLine($"{card.Value} of {card.Suit}");
+            }
+            return handString.ToString();
+        }
+    }
+}
